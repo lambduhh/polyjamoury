c15d6bfcf1e2b31464c9a64872186e17a1728afe
Lorilyn Jordan Miller <47534185+lambduhh@users.noreply.github.com>
2019-10-16 21:31:49 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 # this is a module to handle global constants
 
 client_id = 'b93ebcdfc69847c28ac96b98aabd897f'
-client_secret_id = 'cf77ce53b091435791313b1baaaf0af9'
+client_secret_id = 'bigolebagonope'
 redirect_uri = 'http://google.com/'
 username = '22hfcagpnwds6lyzujw6y2mvi'
 scope = 'user-library-read user-top-read playlist-modify-public user-follow-read'
